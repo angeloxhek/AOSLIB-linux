6b71524bffb90d72993a9483351318f22ff17cdf
angeloxhek <nikitos.antipov.20@gmail.com>
2026-05-02 15:14:32 +0300

    AOSLIB update

--- include/aos/auth.h
+++ include/aos/auth.h
@@ -19,6 +19,10 @@ typedef enum {
 	AUTH_CMD_ADD_USER,
 	AUTH_CMD_DEL_USER,
 	AUTH_CMD_GET_USER_BY_NAME,
+	AUTH_CMD_GET_GROUP,
+	AUTH_CMD_ADD_GROUP,
+	AUTH_CMD_DEL_GROUP,
+	AUTH_CMD_GET_GROUP_BY_NAME,
 } auth_cmd_t;
 
 typedef enum : uint8_t {
@@ -41,8 +45,12 @@ typedef enum : uint8_t {
 #define ATYPE_USER         (ATYPE_CHILD | ATYPE_TOKEN | ATYPE_PASSWORD | ATYPE_CHANGE)
 #define ATYPE_TEMP         (ATYPE_CHILD | ATYPE_TOKEN | ATYPE_PASSWORD)
 
+#define ATYPE_DEFAULT      (ATYPE_CHILD | ATYPE_TOKEN | ATYPE_PASSWORD | ATYPE_CHANGE)
+
 #define APERM_NONE         (0 << 0)
 #define APERM_MANAGE_USER  (1 << 0)
+#define APERM_MANAGE_GROUP (1 << 1)
+#define APERM_USE_GROUP    (1 << 2)
 
 typedef union {
 	struct {
@@ -61,11 +69,31 @@ typedef struct {
 	char pass[64];
 } auth_idex_t;
 
+typedef struct {
+	auth_id_t id;
+	uint8_t auth_type; // ATYPE_*
+	uint64_t deny_perms;
+	uint64_t allow_perms;
+	char name[64];
+	char pass[64];
+} auth_grpex_t; // user perms = user.perms | group.allow_perms & ~group.deny_perms
+
+typedef struct {
+	uint32_t count;
+	auth_id_t data[32];
+} auth_members_t;
+
+#define AUTH_GET_FULL_PERMS(user, group) ((user)->perms | (group)->allow_perms & ~(group)->deny_perms)
+
 void auth_init();
 int auth_get_user(auth_id_t in, auth_idex_t* out);
 int auth_get_user_by_name(const char* in, auth_idex_t* out);
 int auth_add_user(auth_idex_t* inout);
 int auth_del_user(auth_id_t in);
+int auth_get_group(auth_id_t in, auth_grpex_t* out);
+int auth_get_group_by_name(const char* in, auth_grpex_t* out);
+int auth_add_group(auth_grpex_t* inout);
+int auth_del_group(auth_id_t in);
 
 #ifdef __cplusplus
 }
